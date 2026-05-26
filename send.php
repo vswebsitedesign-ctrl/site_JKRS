72ed1ead6bc7e289a1849ca74ee9219c9699d5b6
vswebsitedesign-ctrl <vswebsitedesign@gmail.com>
2026-05-26 07:58:23 +0100

    Fix: send.php corrected to info@houseclearancejkrs.co.uk

--- send.php
+++ send.php
@@ -14,12 +14,14 @@ if (empty($name) || empty($phone) || empty($postcode) || empty($message)) {
     http_response_code(400); echo 'Please fill in all required fields.'; exit;
 }
 
-$to      = 'john@houseclearancejkrs.co.uk';
+$to      = 'info@houseclearancejkrs.co.uk';
 $subject = 'New Enquiry from houseclearancejkrs.co.uk';
 $body    = "Name: $name\nPhone: $phone\nEmail: $email\nPostcode: $postcode\nService: $service\nLocation: $location\n\nMessage:\n$message";
-$headers = "From: noreply@houseclearancejkrs.co.uk\r\nReply-To: $email\r\nX-Mailer: PHP/" . phpversion();
+$headers  = "From: info@houseclearancejkrs.co.uk\r\n";
+$headers .= "Reply-To: " . ($email ?: 'info@houseclearancejkrs.co.uk') . "\r\n";
+$headers .= "X-Mailer: PHP/" . phpversion();
 
-if (mail($to, $subject, $body, $headers)) {
+if (mail($to, $subject, $body, $headers, '-finfo@houseclearancejkrs.co.uk')) {
     header('Location: /thank-you/'); exit;
 } else {
     http_response_code(500); echo 'Mail failed. Please call John directly on 07931 476 744.'; exit;
